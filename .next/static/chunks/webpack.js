2bfa67005b6fc8f074ca0f8ed23cda8c39ece6db
Xiaosong Bi <bixiaosong@gmail.com>
2024-04-26 19:35:26 +0200

    modifie Banner

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8d3feb2852c3daf8"; }
+/******/ 		__webpack_require__.h = function() { return "65a4f740a6794c1f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
